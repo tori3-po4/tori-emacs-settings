20b68b340d4d91015aa80fa53664f166bfe664b0
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-05-07 18:47:07 +0900

    フォントの高さを調整した

--- init.el
+++ init.el
@@ -88,7 +88,7 @@ GUI 以外、または何も見つからなければ nil。"
 (when (and (display-graphic-p) my/main-font)
   (set-face-attribute 'default nil
                       :family my/main-font
-                      :height 150)
+                      :height 170)
   (set-fontset-font t 'unicode
                     (font-spec :family my/main-font)
                     nil 'append)
