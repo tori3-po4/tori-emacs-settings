eedea6bb1a6fe1d01659ff585c77fd2529c72f81
toriyamaiori <iori.wakuwaku.programing@gmail.com>
2026-05-07 19:09:57 +0900

    モードラインの高さを微調整した

--- init.el
+++ init.el
@@ -440,10 +440,10 @@ GUI 以外、または何も見つからなければ nil。"
 (when (and (display-graphic-p) my/main-font)
   (set-face-attribute 'mode-line nil
                       :family my/main-font
-                      :height 150)
+                      :height 170)
   (set-face-attribute 'mode-line-inactive nil
                       :family my/main-font
-                      :height 150))
+                      :height 170))
 
 (when my/icons-available-p
   ;; Nerd Font アイコン領域 (PUA) を専用フォントで描画 ('prepend で最優先)
